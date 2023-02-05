614e248430b98a527e4be63aa7fec6a678afe5e2
mithulix <mohaiminul.devs@gmail.com>
2023-02-05 11:48:39 +0600

    438. Find All Anagrams in a String

---
+++ 438. Find All Anagrams in a String.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {number[]}
+ */
+var findAnagrams = function (s, p) {
+    const chars = new Array(25).fill(0), res = [];
+
+    for (let i = 0; i < p.length; i++) {
+        chars[p.charCodeAt(i) - 97]--;
+    }
+
+    main:
+    for (let i = 0; i < s.length; i++) {
+        chars[s.charCodeAt(i) - 97]++;
+
+        if (i < p.length - 1) continue;
+        if (i > p.length - 1) chars[s.charCodeAt(i - p.length) - 97]--;
+
+        for (let j = 0; j < 25; j++) {
+            if (chars[j]) continue main;
+        }
+        res.push(i - p.length + 1);
+    }
+    return res;
+};
